9f63b25479ecf7838cd12b8bb63b4e3f34942dd4
juliaathar <juathar@gmail.com>
2023-05-31 08:55:23 -0300

    finished Cadastrar method

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using gamer_project_MVC.Infra;
+using gamer_project_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -28,6 +29,22 @@ namespace gamer_project_MVC.Controllers
 
             // retorna a view da equipe (TELA)
             return View();
+        }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Equipe novaEquipe = new Equipe(); // instancia do objeto equipe
+
+            // atribuicao de valores recebidos do formulario 
+            novaEquipe.Nome = form["Nome"].ToString();
+            novaEquipe.Imagem = form["Imagem"].ToString();
+
+            c.Equipe.Add(novaEquipe); // adiciona objeto na tabela do banco de dados
+
+            c.SaveChanges(); // salva alteracoes no banco de dados 
+
+            return LocalRedirect("~/Equipe/Listar"); // retorna para o local chamando a rota de listar (metodo index)
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
